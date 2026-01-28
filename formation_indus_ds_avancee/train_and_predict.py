99a209b1729d313499af7215d806b0a7846f678a
Pierre Kossowski <pkossowski@groupe-eram.com>
2026-01-28 13:13:05 +0000

    clean

--- formation_indus_ds_avancee/train_and_predict.py
+++ formation_indus_ds_avancee/train_and_predict.py
@@ -6,6 +6,10 @@ import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
 
 
+def train_model_with_io(features_path: str, model_registry_folder: str) -> None:
+    features = pd.read_parquet(features_path)
+    train_model(features, model_registry_folder)
+    
 def train_model(features: pd.DataFrame, model_registry_folder: str) -> None:
     target = 'Ba_avg'
     X = features.drop(columns=[target])
@@ -13,17 +17,6 @@ def train_model(features: pd.DataFrame, model_registry_folder: str) -> None:
     model = RandomForestRegressor(n_estimators=1, max_depth=10, n_jobs=1)
     model.fit(X, y)
     joblib.dump(model, os.path.join(model_registry_folder, 'model.joblib'))
-
-
-def predict_with_io(features_path: str, model_path: str, predictions_folder: str) -> None:
-    features = pd.read_parquet(features_path)
-    features = predict(features, model_path)
-    time_str = time.strftime('%Y%m%d-%H%M%S')
-    features['predictions_time'] = time_str
-    features[['predictions', 'predictions_time']].to_csv(os.path.join(predictions_folder, time_str + '.csv'),
-                                                         index=False)
-    features[['predictions', 'predictions_time']].to_csv(os.path.join(predictions_folder, 'latest.csv'), index=False)
-
 
 def predict(features: pd.DataFrame, model_path: str) -> pd.DataFrame:
     model = joblib.load(model_path)
