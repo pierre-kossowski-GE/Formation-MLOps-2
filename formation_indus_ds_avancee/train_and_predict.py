330924265ea61ea0fa286c22582bd570cc8629a4
Pierre Kossowski <pkossowski@groupe-eram.com>
2026-01-28 13:08:34 +0000

    clean

--- formation_indus_ds_avancee/train_and_predict.py
+++ formation_indus_ds_avancee/train_and_predict.py
@@ -5,6 +5,7 @@ import joblib
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
 
+
 def train_model(features: pd.DataFrame, model_registry_folder: str) -> None:
     target = 'Ba_avg'
     X = features.drop(columns=[target])
