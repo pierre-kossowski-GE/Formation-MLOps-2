d80946ce0ac724eb6266e3af0384e6f0d927007c
Pierre Kossowski <pkossowski@groupe-eram.com>
2026-01-29 15:16:27 +0000

    tp6

--- formation_indus_ds_avancee/train_and_predict.py
+++ formation_indus_ds_avancee/train_and_predict.py
@@ -17,12 +17,21 @@ def train_model(features: pd.DataFrame, model_registry_folder: str) -> None:
     target = 'Ba_avg'
     X = features.drop(columns=[target])
     y = features[target]
-    with mlflow.start_run():
-        # insert autolog here ...
-        model = RandomForestRegressor(n_estimators=1, max_depth=10, n_jobs=1)
-        model.fit(X, y)
     time_str = time.strftime('%Y%m%d-%H%M%S')
+    with mlflow.start_run(run_name=f"run_{time_str") as run:
+        # insert autolog here ...
+        mlflow.sklearn.autolog()#log_models = False
+        #mlflow.autolog()
+        model = RandomForestRegressor(n_estimators=1, max_depth=10, n_jobs=1)
+        #model = RandomForestRegressor(n_estimators=n_estimators, max_depth=max_depth, n_jobs=n_jobs)
+        model.fit(X, y)
+        #joblib.dump(model, os.path.join(model_registry_folder, time_str + '.joblib'))
+        mlflow.sklearn.log_model(
+            sk_model=model,
+            artifact_path=os.path.join(model_registry_folder, time_str + '.joblib')   # nom dans les artifacts
+        )
     joblib.dump(model, os.path.join(model_registry_folder, time_str + '.joblib'))
+    
 
 
 def predict_with_io(features_path: str, model_path: str, predictions_folder: str) -> None:
