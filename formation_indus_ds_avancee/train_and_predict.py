9e42581488e3f87b59ebf98c858df0cac40d223a
Pierre Kossowski <pkossowski@groupe-eram.com>
2026-01-29 10:08:11 +0000

    dev tp5

--- formation_indus_ds_avancee/train_and_predict.py
+++ formation_indus_ds_avancee/train_and_predict.py
@@ -1,6 +1,6 @@
 import os
 import time
-
+from datetime import datetime
 import joblib
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
@@ -18,7 +18,10 @@ def train_model(features: pd.DataFrame, model_registry_folder: str) -> None:
     y = features[target]
     model = RandomForestRegressor(n_estimators=1, max_depth=10, n_jobs=1)
     model.fit(X, y)
-    joblib.dump(model, os.path.join(model_registry_folder, 'model.joblib'))
+    
+
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    joblib.dump(model, os.path.join(model_registry_folder+timestamp, 'model.joblib'))
 
 
 def predict_with_io(features_path: str, model_path: str, predictions_folder: str) -> None:
